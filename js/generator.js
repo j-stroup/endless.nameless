cd3ba43d24fbd2da434b56d462df9a69aa939383
j-stroup <jeffreyrstroup@gmail.com>
2024-01-13 09:21:58 -0500

    Making progress

--- js/generator.js
+++ js/generator.js
@@ -1,11 +1,19 @@
 function generateRandomHTML() {
-  const tags = ['div', 'p', 'h1', 'h3', 'h5', 'span', 'button'];
-  const attributes = ['id', 'class', 'style'];
+  var arr = [];
+  for (let step = 0; step <= 500; step++) {
+    const tags = ['div', 'p', 'h1', 'h3', 'h5', 'span'];
+    const attributes = ['id', 'class', 'style'];
 
-  const randomTag = tags[Math.floor(Math.random() * tags.length)];
-  const randomAttribute = attributes[Math.floor(Math.random() * attributes.length)];
+    var randomTag = tags[Math.floor(Math.random() * tags.length)];
+    var randomAttribute = attributes[Math.floor(Math.random() * attributes.length)];
 
-  const htmlCode = `<${randomTag} ${randomAttribute}="value">Content</${randomTag}>`;
+    var htmlCode = `<${randomTag} ${randomAttribute}="value">Content</${randomTag}>`;
 
-  document.getElementById("html").innerHTML = htmlCode;
+    arr.push(htmlCode);
+  }
+    let codes = '';
+    for (var i = 0; i < arr.length; i++) {
+      codes = codes + arr[i];
+    }
+    document.getElementById("html").innerHTML = codes;
 }
